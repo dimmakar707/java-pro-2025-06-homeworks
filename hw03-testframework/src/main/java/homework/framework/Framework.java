18451603e480106f40108da6b6168429b2f05716
Dmitriy Makarenkov <sitesun707@gmail.com>
2025-08-10 16:27:59 +0300

    ДЗ 3 исправлено

--- hw03-testframework/src/main/java/homework/framework/Framework.java
+++ hw03-testframework/src/main/java/homework/framework/Framework.java
@@ -6,13 +6,17 @@ import homework.annotations.Test;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Framework {
-    private static final List<Method> beforeMethods = new ArrayList<>();
-    private static final List<Method> testedMethods = new ArrayList<>();
-    private static final List<Method> afterMethods = new ArrayList<>();
+    private static final Logger logger = LoggerFactory.getLogger(Framework.class);
 
     public static void run(String className) {
+
+        final List<Method> beforeMethods = new ArrayList<>();
+        final List<Method> testedMethods = new ArrayList<>();
+        final List<Method> afterMethods = new ArrayList<>();
 
         try {
             Class<?> testClass = Class.forName(className);
@@ -41,37 +45,36 @@ public class Framework {
                         ReflectionHelper.callMethod(classInstance, beforeMethod.getName());
                     } catch (Exception e) {
                         failBeforeTests++;
-                        System.out.println("Exception in @before-method: " + beforeMethod.getName());
+                        logger.info("Exception in @before-method: {}", beforeMethod.getName());
                     }
                 }
 
                 if (failBeforeTests > 0) {
                     failTest++;
-                    continue;
-                }
-
-                try {
-                    ReflectionHelper.callMethod(classInstance, method.getName());
-                    successTest++;
-                } catch (Exception e) {
-                    failTest++;
-                    System.out.println("Exception in method: " + method.getName());
+                } else {
+                    try {
+                        ReflectionHelper.callMethod(classInstance, method.getName());
+                        successTest++;
+                    } catch (Exception e) {
+                        failTest++;
+                        logger.info("Exception in method: {}", method.getName());
+                    }
                 }
 
                 for (Method afterMethod : afterMethods) {
                     try {
                         ReflectionHelper.callMethod(classInstance, afterMethod.getName());
                     } catch (Exception e) {
-                        System.out.println("Exception in @after-method: " + afterMethod.getName());
+                        logger.info("Exception in @after-method: {}", afterMethod.getName());
                     }
                 }
             }
 
-            System.out.println("-------------------------------");
-            System.out.println("Test is completed");
-            System.out.println("Success tests count: " + successTest);
-            System.out.println("Failed tests count: " + failTest);
-            System.out.println("-------------------------------");
+            logger.info("-------------------------------");
+            logger.info("Test is completed");
+            logger.info("Success tests count: {}", successTest);
+            logger.info("Failed tests count: {}", failTest);
+            logger.info("-------------------------------");
 
         } catch (Exception e) {
             System.out.println("Exception in framework: " + e.getMessage());
